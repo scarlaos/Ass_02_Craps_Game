95e88b053e70310de9a0c88afda1cc3d07679e46
scarlaos <scarlaos@mail.uc.edu>
2024-10-14 14:24:47 -0400

    init

--- src/Craps_Game.java
+++ src/Craps_Game.java
@@ -1,7 +1,7 @@
 import java.util.Random;
 import java.util.Scanner;
 
-public class Main {
+public class Craps_Game {
     public static void main(String[] args)
     {
         Random rnd = new Random();
